58e437249cccc4b13d3be3a33a967a11bbf90458
Andrey Lamzin <lamzin.an@gmail.com>
2019-10-29 12:33:04 +0300

    Включен кэш sw

--- public/js/webpack.mix.js
+++ public/js/webpack.mix.js
@@ -13,6 +13,10 @@ let mix = require('laravel-mix');
 
 //Страницы сайта - показ телефонов и фильтры
 mix.js('sources/site/app.js', 'L.js');
+
+//Страницы сайта на которых надо только правильно работать с кэшем
+mix.js('sources/site/cacheonlyapp.js', 'co.js');
+
 /*mix.styles([
 		'./../s/vendor/bootstrap4.2.1.min.css',
 		'./../s/vendor/fontawesome5/all.css',
