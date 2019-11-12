f6d2f56a88df5a57f63c59a6eb90f5a410240dd6
Andrey Lamzin <lamzin.an@gmail.com>
2019-11-12 12:03:51 +0300

    Движение в сторону google каптчи, отправляем письмо без каптчи

--- src/Controller/ResettingController.php
+++ src/Controller/ResettingController.php
@@ -90,9 +90,9 @@ class ResettingController extends AbstractController
      */
     public function sendEmailAction(Request $request)
     {
-		$cp = $request->get('cp');
+		/*$cp = $request->get('cp');
 		var_dump($cp);
-		die;
+		die;*/
         return $this->resettingController->sendEmailAction($request);
     }
 
